08ee3bb7b88a1359973188042b2f9195cc8efa51
akash khandelwal <akashkhandelwal1999@gmail.com>
2021-02-11 12:00:25 +0530

    Updates

---
+++ precache-manifest.5eb9187f2abc3c1b017226f37da2de95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6835cb5e09078c162f87cd6cb9b060ca",
+    "url": "/burgerBuilder/index.html"
+  },
+  {
+    "revision": "928be010b4d59929ed4b",
+    "url": "/burgerBuilder/static/css/main.9ad9f3d9.chunk.css"
+  },
+  {
+    "revision": "e70a21ae49732b36d548",
+    "url": "/burgerBuilder/static/js/2.0226aeb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burgerBuilder/static/js/2.0226aeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928be010b4d59929ed4b",
+    "url": "/burgerBuilder/static/js/main.7eda49ef.chunk.js"
+  },
+  {
+    "revision": "ad048b0481723bedbe20",
+    "url": "/burgerBuilder/static/js/runtime-main.6346bb26.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
